06791ccd56a3c3594aad6b2f556bf90cbb9a1496
MAHA <MAHA@DESKTOP-GUFMCJ1>
2022-04-11 21:22:18 +0530

    commit

---
+++ LambdaExpression/src/com/edu/FunctionInterfaceEx.java
@@ -0,0 +1,18 @@
+package com.edu;
+
+import java.util.Scanner;
+import java.util.function.Function;
+
+public class FunctionInterfaceEx {
+
+	public static void main(String[] args) {
+		Function< String,Integer> len=(str)->str.length();
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter a string");
+		String s=sc.nextLine();
+		int l=len.apply(s);
+		System.out.println("length of the String ="+l);
+		
+	}
+
+}
